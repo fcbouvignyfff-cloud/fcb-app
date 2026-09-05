175748c6188f5a87a4577c0f0b23dae828f50a30
Bouvigny <fcbouvignyfff@gmail.com>
2026-09-05 22:47:52 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+
+const CACHE = 'fcb-shell-v1';
+const SHELL = ['./','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+  if(url.origin !== self.location.origin) return;
+
+  if(event.request.mode === 'navigate'){
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./', copy));
+          return response;
+        })
+        .catch(() => caches.match('./'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+    )
+  );
+});
