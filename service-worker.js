ed0c74d95963f3f57287995fd6151cf8498f4417
Bouvigny <fcbouvignyfff@gmail.com>
2026-09-05 23:16:01 +0200

    Implement service worker with caching strategies

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,72 @@
+const CACHE = 'fcb-final-v3';
 
+const SHELL = [
+  './',
+  './manifest.json',
+  './club-logo.png',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(SHELL))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) return;
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put('./', copy);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match('./'))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      });
+    })
+  );
+});
